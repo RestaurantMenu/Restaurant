a083b8368f2a25ae7be8d054ce8bab23aeceb4d9
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-07-26 12:31:15 +0530

    Total chaging script added to Image_Blur.js

--- Image_Blur.js
+++ Image_Blur.js
@@ -1,7 +1,14 @@
 function foods(name){
+    
+    var total = 0;
+    
     if(document.getElementById(name).style.filter == "blur(5px)"){
         document.getElementById(name).style.filter = "blur(0px)";
+        if(total>0){
+            total = total - 100;
+        }
     }else{
         document.getElementById(name).style.filter = "blur(5px)";
+        total = total + 100;
     }      
 }
